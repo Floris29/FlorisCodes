53730fcc991d3f4bde8e4e7c43c6a9052a79c98d
Floris29 <Florismelchers@gmail.com>
2024-09-24 16:55:07 +0200

    Project is set up

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,10 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('FlorisCodes', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite('resources/css/app.css')
-
 
 </head>
 
