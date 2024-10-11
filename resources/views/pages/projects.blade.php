9c5a6e590887ec372b70af9d9e0e6faad38ec8e4
Floris29 <Florismelchers@gmail.com>
2024-10-11 15:58:08 +0200

    removed header as component and just added it as div

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -1,12 +1,16 @@
 @extends('components.layout')
-<x-c-r-u-dheader />
 
 @section('content')
+    <div class="uppercase text-subtitle font-antonio font-bold flex justify-between items-center">
+        <a href="/projects/create" class="p-4 text-5xl">Create a new project</a>
+    </div>
+
+
     <div class="">
         @foreach ($projects as $index => $project)
             <div class="flex {{ $index % 2 == 0 ? 'flex-row' : 'flex-row-reverse' }} items-center justify-center gap-52">
                 <div>
-                    <img src="{{ asset('storage/' . $project->image_path) }}" alt="{{ $project->name }}" > 
+                    <img src="{{ asset('storage/' . $project->image_path) }}" alt="{{ $project->name }}">
                 </div>
                 <div>
                     <h1 class="uppercase text-4xl font-antonio font-bold text-title">{{ $project->name }}</h1>
