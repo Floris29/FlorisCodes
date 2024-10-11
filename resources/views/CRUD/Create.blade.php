3de12efdecd70d617a264dec72bb6142c908d90a
Floris29 <Florismelchers@gmail.com>
2024-10-11 17:41:58 +0200

    styled the create form

--- resources/views/CRUD/Create.blade.php
+++ resources/views/CRUD/Create.blade.php
@@ -1,7 +1,7 @@
 @extends('components.layout')
 
 @section('content')
-    <h1>Create a new project</h1>
+    <h1 class="text-7xl text-title uppercase font-antonio font-bold leading-none text-center">Create a new project</h1>
 
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -13,18 +13,21 @@
         </div>
     @endif
 
-    <form action="{{ route('projects.store') }}" method="POST" enctype="multipart/form-data">
+
+    <form action="{{ route('projects.store') }}" method="POST" enctype="multipart/form-data"
+        class="bg-icon flex flex-col flex-wrap-reverse content-center w-1/2 mx-auto mt-10 text-text text-xl border rounded-md">
         @csrf
-        <label for="name">Project name</label>
-        <input type="text" name="name" id="name" required>
 
-        <label for="description">Project description</label>
-        <textarea name="description" id="description" required></textarea>
+        <input type="text" name="name" id="name" placeholder="Project name"
+            class="uppercase font-antonio font-bold text-center placeholder:text-text m-5 p-2 bg-icon border rounded-md" required>
 
-        <label for="image">Project image</label>
-        <input type="file" name="image" id="image" required>
+        <textarea name="description" id="description" placeholder="Project description"
+            class="uppercase font-antonio font-bold text-center p-2 m-5 bg-icon placeholder:text-text border rounded-md" rows="4" required></textarea>
 
-        <button type="submit">Create project</button>
+        <input type="file" name="image" id="image"
+            class="uppercase font-antonio font-bold  text-center m-5 bg-icon p-2" required>
 
+        <button type="submit" class="uppercase font-antonio font-bold text-center m-5 p-2 bg-icon border rounded-md">Create project</button>
     </form>
+    <a href="/projects"><h2 class="text-4xl text-subtitle uppercase font-antonio font-bold leading-none text-center mt-5">Go back to projects</h2></a>
 @endsection
