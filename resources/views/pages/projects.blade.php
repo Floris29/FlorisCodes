159b0de35e0d0acb0298c1ca8d33bd443e271b0b
Floris29 <Florismelchers@gmail.com>
2024-10-11 16:19:57 +0200

    edit button is on hover, link working now

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -8,13 +8,15 @@
 
     <div class="">
         @foreach ($projects as $index => $project)
-            <div class="flex {{ $index % 2 == 0 ? 'flex-row' : 'flex-row-reverse' }} items-center justify-center gap-52">
+            <div class="flex {{ $index % 2 == 0 ? 'flex-row' : 'flex-row-reverse' }} items-center justify-center group">
                 <div>
                     <img src="{{ asset('storage/' . $project->image_path) }}" alt="{{ $project->name }}">
                 </div>
                 <div>
                     <h1 class="uppercase text-4xl font-antonio font-bold text-title">{{ $project->name }}</h1>
                     <p class="font-antonio text-text">{{ $project->description }}</p>
+                    <a href="{{ route('projects.edit', $project->id) }}" class="font-antonio text-text hidden group-hover:block">Edit project</a>
+
                 </div>
             </div>
         @endforeach
