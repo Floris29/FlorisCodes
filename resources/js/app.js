1588b2dd4e68b6f5505cd326abfd02024157c998
Floris29 <Florismelchers@gmail.com>
2024-10-14 10:30:43 +0200

    added contact section

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
 import '../css/app.css'; 
 
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-    }, 1000);
-    const me = document.querySelector('#me');
-    me.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-    }); 
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     setTimeout(() => {
+//     }, 1000);
+//     const me = document.querySelector('#me');
+//     me.scrollIntoView({
+//         behavior: 'smooth',
+//         block: 'center',
+//     }); 
+// });
