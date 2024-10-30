a85fdc2f4b83d210c259f951e45faeb368d7a5b4
Floris29 <Florismelchers@gmail.com>
2024-10-30 13:54:00 +0100

    making the local work with the server

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,9 +9,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BuP_n9yT.css') }}">
-    <script src="{{ asset('build/assets/app-D8Z59LYo.js') }}" defer></script>
-    <link rel="favicon" href="{{ asset('../img/favicon.png') }}" type="image/x-icon" />
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link rel="favicon" href="{{ asset('img/favicon.png') }}" type="image/x-icon" />
     <script src="https://kit.fontawesome.com/aab1cdc4fa.js" crossorigin="anonymous"></script>
 
 </head>
