2d8364a3080795fd37ae9b25a1ac078f2ef7a93d
Floris29 <Florismelchers@gmail.com>
2024-10-30 22:55:05 +0100

    removed unneccesery icon

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -28,11 +28,11 @@
             class="fixed top-0 left-0 h-full w-64 bg-gray-800  p-5 transition-transform duration-300 transform 2xl:hidden xl:hidden lg:hidden">
             <div class="flex justify-end">
                 <button @click="open = false" class=" text-xl">
-                    <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    {{-- <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
                         </path>
-                    </svg>
+                    </svg> --}}
                 </button>
             </div>
 
