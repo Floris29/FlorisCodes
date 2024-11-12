f1763fcf1cd7bb85a6a738a246f9dc5a0f91e525
Floris29 <Florismelchers@gmail.com>
2024-11-12 13:19:59 +0100

    page setup

--- resources/views/admin/adminProjects.blade.php
+++ resources/views/admin/adminProjects.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Projects') }}
         </h2>
     </x-slot>
 
@@ -14,6 +14,7 @@
                         class="uppercase text-subtitle p-4 text-5xl font-antonio font-bold flex justify-between items-center">
                         <a href="/projects/create">Create a new project</a>
                         <a href="/">Back to the home page</a>
+                        <a href="/dashboard">Back to the dashboard</a>
                     </div>
 
 
