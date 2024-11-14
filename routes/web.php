a0dffbc14eaa85e7624cb18c5999fedc3c30634c
Floris29 <Florismelchers@gmail.com>
2024-11-14 22:10:43 +0100

    filament add and file cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProjectController;
 use App\Models\Project;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,38 +15,6 @@ use App\Models\Project;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
     $projects = Project::latest()->limit(3)->get();
     return view('pages.home', compact('projects'));
 });
-
-Route::resource('projects', ProjectController::class);
-
-// Route::get('/dashboard', function () {
-//     return view('admin.dashboard'); 
-// })->middleware(['auth', 'role:admin'])->name('dashboard');
-
-// Route::get('/adminprojects', function () {
-//     $projects = Project::latest()->get();
-//     return view('admin.adminprojects', compact('projects'));
-// })->middleware(['auth', 'role:admin'])->name('dashboard');
-
-// Route::get('/users', function () {
-//     return view('admin.users'); 
-// })->middleware(['auth', 'role:admin'])->name('users');
-
-// Route::get('/statistics', function () {
-//     return view('admin.statistics'); 
-// })->middleware(['auth', 'role:admin'])->name('statistics');
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
-
