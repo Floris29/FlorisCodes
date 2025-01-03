3163d8d5510c987a04dba1d0f0e682c195b9a151
Floris29 <Florismelchers@gmail.com>
2025-01-03 13:35:52 +0100

    responsibility fix

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -12,7 +12,7 @@
             @foreach ($projects as $project)
                 <div
                     class="flex {{ $loop->index % 2 == 0 ? 'flex-row' : 'flex-row-reverse' }} items-center justify-center 
-                    group gap-8 lg:gap-8 md:gap-7 sm:block xs:block p-5 bg-primary-bg w-2/3 mx-auto mt-8">
+                    group gap-8 lg:gap-8 md:gap-7 sm:block xs:block p-5 bg-primary-bg w-2/3 xs:w-full sm:w-full mx-auto mt-8">
 
                     <div>
                         <img src="{{ asset('storage/public/' . $project->image_path) }}" alt="{{ $project->name }}"
@@ -28,7 +28,7 @@
                             @if ($project->skills_used)
                                 @foreach ($project->skills_used as $skill)
                                     <div
-                                        class="px-6 py-3 mb-3 mr-5 text-lg bg-icon font-antonio font-bold text-text lg:px-4 lg:py-2 md:py-2 sm:px-4 sm:py-2 xs:px-4 xs:py-2">
+                                        class="px-6 py-3 mb-3 mr-5 text-lg bg-icon font-antonio font-bold text-text lg:px-4 lg:py-2 md:py-2 sm:px-4 sm:py-2 xs:px-4 xs:py-2 xs:w-1/2 sm:w-1/2">
                                         <p>{{ $skill }}</p>
                                     </div>
                                 @endforeach
@@ -38,15 +38,16 @@
                         <p class="text-xl md:text-lg sm:text-md font-antonio xs:text-md text-text lg:text-lg">
                             {{ $project->description }}</p>
 
-                        <div class="flex mt-3 md:justify-between sm:justify-between xs:justify-between">
-                            <a href="{{ $project->url }}" target="_blank"
-                                class="px-6 py-3 mr-5 text-lg bg-icon font-antonio font-bold text-text uppercase lg:px-4 lg:py-2 md:py-2 sm:px-4 sm:py-2 xs:px-4 xs:py-2">Visit
-                                Site</a>
+                        <div class="flex mt-3 md:justify-between sm:justify-center xs:justify-center">
+                            @if ($project->url)
+                                <a href="{{ $project->url }}" target="_blank"
+                                    class="px-6 py-3 mr-5 text-lg bg-icon font-antonio font-bold text-text uppercase lg:px-4 lg:py-2 md:py-2 sm:px-4 sm:py-2 xs:px-4 xs:py-2">Visit
+                                    Site</a>
+                            @endif
                             <a href="{{ $project->github }}" target="_blank"
                                 class="px-6 py-3 mr-5 text-lg bg-icon font-antonio font-bold text-text uppercase lg:px-4 lg:py-2 md:py-2 sm:px-4 sm:py-2 xs:px-4 xs:py-2">View
                                 Code</a>
                         </div>
-
                     </div>
                 </div>
             @endforeach
